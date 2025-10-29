22cb291c5f6347476a9b8b4badd75eab70e485d7
AliMT25 <hutu_putu_@hotmail.com>
2025-10-29 22:01:30 +0300

    Araç Sepeti V1

---
+++ AracSepetiV1/footer.php
@@ -0,0 +1,3 @@
+</div> <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7VCAuApddCyUuKbYmPAYxT0K5E" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ AracSepetiV1/sayfalar/register.php
@@ -0,0 +1,33 @@
+<div class="row justify-content-center">
+    <div class="col-md-5">
+        <div class="card">
+            <div class="card-body">
+                <h1 class="card-title text-center mb-4">Üye Ol</h1>
+                <form>
+                    <div class="mb-3">
+                        <label for="nameInput" class="form-label">Adınız</label>
+                        <input type="text" class="form-control" id="nameInput">
+                    </div>
+                    <div class="mb-3">
+                        <label for="surnameInput" class="form-label">Soyadınız</label>
+                        <input type="text" class="form-control" id="surnameInput">
+                    </div>
+                    <div class="mb-3">
+                        <label for="emailInput" class="form-label">E-posta Adresi</label>
+                        <input type="email" class="form-control" id="emailInput" placeholder="ornek@mail.com">
+                    </div>
+                    <div class="mb-3">
+                        <label for="passwordInput" class="form-label">Şifre</label>
+                        <input type="password" class="form-control" id="passwordInput">
+                    </div>
+                    <div class="d-grid">
+                        <button type="submit" class="btn btn-success">Kayıt Ol</button>
+                    </div>
+                    <p class="text-center mt-3">
+                        Zaten hesabın var mı? <a href="index.php?sayfa=login">Giriş Yap</a>
+                    </p>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
